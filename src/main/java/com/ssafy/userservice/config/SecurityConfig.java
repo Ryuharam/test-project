eff887b96a8456e0da4edf1020968b43ab6bcb4b
유하람 <dlfandl97@naver.com>
2025-01-14 17:35:17 +0900

    securityConfig 작성중

---
+++ src/main/java/com/ssafy/userservice/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.ssafy.userservice.config;
+
+import jakarta.servlet.Filter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        return http.build(); // 최종 SecurityFilterChain 반환
+    }
+
+
+
+}
---
+++ src/main/java/com/ssafy/userservice/user/User.java
@@ -0,0 +1,54 @@
+package com.ssafy.userservice.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "USERS")
+@Getter
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "USER_ID")
+    private Long id;
+
+    @Column(name="email",nullable = false, unique = true)
+    private String email;
+
+    @Column(name="name",nullable = false)
+    private String name;
+
+    @Column(name="password",nullable = false)
+    private String password;
+
+    @Column(name="provider", nullable = false)
+    private String provider;
+
+    @Column(name="created_at",nullable = true)
+    @CreatedDate
+    private Date created_at;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+}
